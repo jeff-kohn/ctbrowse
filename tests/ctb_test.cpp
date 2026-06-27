2c20507abbb2085494519203f87cec5ce944616e
Jeff Kohn <noreply@github.com>
2026-06-26 22:16:07 -0500

    ci build fix

--- tests/ctb_test.cpp
+++ tests/ctb_test.cpp
@@ -1,36 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
-#include <ctb/download.h>
-
-#include <chrono>
-#include <print>
-
 
 namespace ctb::tests
 {
 
-   using namespace std::literals;
-
-
-   TEST_CASE("download_table", "[rest_helpers]")
-   {
-      CredentialWrapper cred{ "test", "", "" };
-
-      //downloadTableAsync(
-      //   cred,
-      //   [](DownloadResult result)
-      //   {
-      //      if (result)
-      //      {
-      //         std::println("Got table {}, size {} bytes.", result->tableName(), result->data.size());
-      //      }
-      //      else
-      //      {
-      //         std::println("Got an error! {}", result.error().formattedMessage());
-      //      }
-      //   },
-      //   TableId::Pending);
-
-      std::this_thread::sleep_for(1s);
-   }
 
 }   // namespace ctb::tests
